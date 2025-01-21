ace97e7f69d669831c3ddad6ef64e9c80a5505ed
AdamCMunro <adammunro5609@gmail.com>
2025-01-21 17:06:19 +0000

    added insert card image

--- insert_card_form.Designer.cs
+++ insert_card_form.Designer.cs
@@ -28,22 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(insert_card_form));
+            this.insert_card_picture_box = new System.Windows.Forms.PictureBox();
+            this.insert_card_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.insert_card_picture_box)).BeginInit();
             this.SuspendLayout();
+            // 
+            // insert_card_picture_box
+            // 
+            this.insert_card_picture_box.Image = ((System.Drawing.Image)(resources.GetObject("insert_card_picture_box.Image")));
+            this.insert_card_picture_box.InitialImage = ((System.Drawing.Image)(resources.GetObject("insert_card_picture_box.InitialImage")));
+            this.insert_card_picture_box.Location = new System.Drawing.Point(404, 237);
+            this.insert_card_picture_box.Name = "insert_card_picture_box";
+            this.insert_card_picture_box.Size = new System.Drawing.Size(499, 501);
+            this.insert_card_picture_box.TabIndex = 0;
+            this.insert_card_picture_box.TabStop = false;
+            // 
+            // insert_card_label
+            // 
+            this.insert_card_label.AutoSize = true;
+            this.insert_card_label.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insert_card_label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(120)))), ((int)(((byte)(219)))));
+            this.insert_card_label.Location = new System.Drawing.Point(543, 152);
+            this.insert_card_label.Name = "insert_card_label";
+            this.insert_card_label.Size = new System.Drawing.Size(213, 50);
+            this.insert_card_label.TabIndex = 1;
+            this.insert_card_label.Text = "Insert Card";
             // 
             // insert_card_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(937, 521);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ClientSize = new System.Drawing.Size(1332, 973);
+            this.Controls.Add(this.insert_card_label);
+            this.Controls.Add(this.insert_card_picture_box);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "insert_card_form";
             this.Text = "Insert Card";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.insert_card_form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.insert_card_picture_box)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox insert_card_picture_box;
+        private System.Windows.Forms.Label insert_card_label;
     }
 }
