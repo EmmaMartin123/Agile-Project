e81fb746db3e9c0e1e10bd00d84e0396ad5d44a7
Marylou <2501402@dundee.ac.uk>
2025-01-29 18:55:23 +0000

    added a diaplay prompt for offer receipt

--- ATM/prompt_form.cs
+++ ATM/prompt_form.cs
@@ -13,18 +13,33 @@ namespace ATM_forms
 {
     public partial class PromptForm : Form
     {
-        public PromptForm()
+        public PromptForm(string title, string message)
         {
             InitializeComponent();
+            this.Text = title;
+            this.StartPosition = FormStartPosition.CenterScreen; // ensures it starts centered
+            prompt_label1.Text = message;
+            // position text dynamically based on length
+            prompt_label1.Left = (this.ClientSize.Width - prompt_label1.Width) / 2;
         }
 
 
         private void YesbtnClick(object sender, EventArgs e)
         {
-
+            this.Close(); 
         }
 
         private void NobtnClick(object sender, EventArgs e)
+        {
+            this.Close(); 
+        }
+
+        private void PromptForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void prompt_label1_Click(object sender, EventArgs e)
         {
 
         }
